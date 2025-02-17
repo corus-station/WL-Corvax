74489521c0c73b909ff10c052232843bfc115a5b
Livsi <142896379+DocLivsiii@users.noreply.github.com>
2025-02-17 12:44:37 +0500

    Delete Resources/Prototypes/_WL/Medical Records Console directory

--- Resources/Prototypes/_WL/Medical Records Console/SharedMedicalRecordsConsoleSystem.cs
+++
@@ -1,6 +0,0 @@
-namespace Content.Shared.MedicalRecords.Systems;
-
-/// <summary>
-/// Station records aren't predicted, just exists for access.
-/// </summary>
-public abstract class SharedMedicalRecordsConsoleSystem : EntitySystem;
